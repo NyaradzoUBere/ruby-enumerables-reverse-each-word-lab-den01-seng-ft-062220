61dae573932c22cedc6e2e451d79caf3f2a3d6de
Nyaradzo Bere <nyaradzo.bere@colorado.edu>
2020-06-12 21:46:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,11 @@
 greeting = "Hello, there, and how are you?"
-def reverse_each_word
-  greeting = greeting.split()
-  greeting.each do |words|
-    greeting.reverse
-  end
-end
 
-puts reverse_each_word
+greeting = greeting.split()
+# def reverse_each_word
+#   greeting = greeting.split()
+#   greeting.each do |words|
+#     greeting.reverse
+#   end
+# end
+
+# puts reverse_each_word
